a1f6c2a61dd8159268269d3164a50cb11f297d5f
Haikal F Luzain <haikalfikriluzain@gmail.com>
2019-06-07 18:44:20 +0700

    Update

--- database/migrations/2019_01_09_115945_create_todays_table.php
+++ database/migrations/2019_01_09_115945_create_todays_table.php
@@ -14,7 +14,7 @@ class CreateTodaysTable extends Migration
     public function up()
     {
         Schema::create('todays', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('activity');
             $table->string('start');
             $table->string('end');
